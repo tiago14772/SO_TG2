d25c4d50fa62dd76dbe464aaf23c56b68e6650b8
lviana <lviana@Lviana.myguest.virtualbox.org>
2024-01-25 13:40:34 +0000

    24/01/2024 - 18:30 - Requisitos básicos cumpridos, corrigidos alguns erros dos semaforos e menus.

--- main.c
+++ main.c
@@ -1,3 +1,14 @@
+/**
+ * @authors Luis Viana, nº 5327
+ * @authors Tiago Francisco, nº 14772
+ *
+ * @date 25/01/2024
+ *
+ * @version 1
+ * gcc -o tg2 tg2_5327_14772.c -pthread
+ * ./tg2
+ */
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -13,9 +24,9 @@
 #define MAX_MAP_DESCRIPTION 200
 #define NO_WAY (-1)
 
-static int CURRENT_PLACE = 0;
-char op[10];
-sem_t sem;
+static int CURRENT_PLACE = 0; // Declare a static variable for the current player position on the map
+char op[10]; // Declare a character array named "op" with a size of 10
+sem_t sem; // Declare a semaphore named "sem"
 
 /*
  * Struct that represents the objects loaded by the players
@@ -37,7 +48,6 @@ struct Monster {
 
 /*
  * Struct that represents the player in the game
- *
  * */
 struct Player {
     char name[MAX_NAME];
@@ -63,61 +73,85 @@ struct Map {
     char info[MAX_NAME];
 };
 
+/*
+ * Struct that represents the overall game data
+ * Player, Monsters and Objects
+ * */
 struct Data {
     struct Player player;
     struct Monster monster[1];
     struct Object objects[3];
 };
 
-static struct Map map[100];
+static struct Map map[16];
 
+void menu();
 void playerInitialize(struct Player *player);
-void mapInitialize();
 void monsterInitialize(struct Monster monster[]);
 void objectsInitialize(struct Object object[]);
+void mapInitialize();
 void processPlayerChoice(struct Data *d);
+void changeRoom(const char *direction, struct Data *d);
 void fightMonster(struct Data *d);
 void checkRoom(struct Data *d);
 void playerResume(struct Data *d);
 void *threadPlayer(void *ptr);
 void *threadMonster(void *ptr);
-void menu();
 
-int main(int argc, char *argv[]) {
-
+ /**
+ * @brief Main function to start the game.
+ *
+ * This function initializes game entities, displays the menu, creates threads
+ * for player and monster, and waits for them to finish before exiting.
+ *
+ * @return Returns 0 on successful execution.
+ */
+int main() {
+    // Declare variables
     struct Monster monster[1];
     struct Player player;
     struct Object objects[3];
     struct Data d;
 
+    // Display the game menu
     menu();
-
+    // Initialize player, map, objects, and monsters
     playerInitialize(&player);
     mapInitialize();
     objectsInitialize(objects);
     monsterInitialize(monster);
 
+    // Initialize semaphore for synchronization of threads
     sem_init(&sem, 1, 1);
 
+    // Populate game data structure with initialized objects
     d.player = player;
     d.monster[0] = monster[0];
     d.objects[0] = objects[0];
     d.objects[1] = objects[1];
     d.objects[2] = objects[2];
 
+    // Declare threads for player and monster
     pthread_t playerThread, monsterThread;
 
+    // Initialize the command string
     strcpy(op, "");
 
+    // Create threads for player and monster, passing game data structure
     pthread_create(&playerThread, NULL, threadPlayer, (void *) &d);
     pthread_create(&monsterThread, NULL, threadMonster, (void *) &d);
 
+    // Wait for player and monster threads to finish
     pthread_join(playerThread, NULL);
     pthread_join(monsterThread, NULL);
 
+    // Exit the program
     return 0;
 }
 
+/*
+ * Function to display the game menu
+ * */
 void menu() {
     printf("#####################################################\n"
            "#                      WELCOME                      #\n"
@@ -153,7 +187,7 @@ void playerInitialize(struct Player *player) {
 }
 
 /**
- * @brief Initializes the array of monsters (enemies) in the game.
+ * @brief Initializes the array of monsters in the game.
  *
  * This function sets up the initial values for the monsters in the game.
  * It assigns names, energy, power, and map locations to each monster in the 'monster' array.
@@ -333,12 +367,86 @@ void mapInitialize() {
         map[i].treasure = NO_TREASURE;
         map[i].object = NO_OBJECT;
 
-        char desc[50];
+        char desc[64];
         sprintf(desc, "################# You are in Room %d #################", i + 1);
         strcpy(map[i].description, desc);
     }
     pathInitialize();
     setInfoCells();
+}
+
+/**
+ * @brief Thread function representing player actions.
+ *
+ * This function continuously processes player actions until the "q" command is received.
+ * It checks if the player is on the same map as the monster and initiates a fight if true.
+ * After processing player choices, it continues the loop.
+ *
+ * @param ptr A pointer to the game data structure (struct Data).
+ * @return NULL (void pointer) as it is a pthread-compatible function.
+ */
+void *threadPlayer(void *ptr) {
+    // Cast the generic pointer to a pointer of struct Data
+    struct Data *d = (struct Data *) ptr;
+    while (strcmp(op, "q") != 0) {
+        // Acquire the semaphore to ensure exclusive access to shared data
+        sem_wait(&sem);
+        if (d->player.map == d->monster[0].map){
+            fightMonster(d);
+        }
+        // Release the semaphore after accessing shared data
+        sem_post(&sem);
+        processPlayerChoice(d);
+    }
+    // Return NULL as it is a pthread-compatible function
+    return NULL;
+}
+
+/**
+ * @brief Process player choices based on the current game state.
+ *
+ * This function displays information about the current room, prompts the player for choices,
+ * and performs corresponding actions based on the player's input.
+ *
+ * @param d A pointer to the game data structure (struct Data).
+ */
+void processPlayerChoice(struct Data *d) {
+    // Acquire the semaphore to ensure exclusive access to shared data
+    sem_wait(&sem);
+    // Display information about the current room and player options
+    printf("\n%s", map[CURRENT_PLACE].description);
+    printf("\nPlayer info(i), search Room(f)\n");
+    // Display additional information about the current room directions
+    printf("%s", map[CURRENT_PLACE].info);
+    scanf("%s", op);
+    // Release the semaphore after accessing shared data
+    sem_post(&sem);
+    // Show resume player information
+    if (strcmp(op, "i") == 0) {
+        playerResume(d);
+        return;
+    }
+    // Check if there are any object or treasure in the room
+    if (strcmp(op, "f") == 0) {
+        checkRoom(d);
+        return;
+    }
+    // Quit the game
+    if (strcmp(op, "q") == 0) {
+        exit(0);
+    }
+    sleep(1);
+    // Check if the player is in the same room as the monster
+    // in case of true, go back to start a fight
+    if (d->player.map == d->monster[0].map){
+        return;
+    }
+    // Acquire the semaphore to ensure exclusive access to shared data
+    sem_wait(&sem);
+    // Go to the method to check if it'ś possible to change the room based on the player's choice
+    changeRoom(op, d);
+    // Release the semaphore after accessing shared data
+    sem_post(&sem);
 }
 
 /**
@@ -376,100 +484,26 @@ void changeRoom(const char *direction, struct Data *d) {
 }
 
 /**
- * @brief Processes the player's choice of direction.
+ * @brief Thread function representing monster actions.
  *
- * This function prompts the player to choose a direction and then calls the changeRoom function
- * to handle the actual room change based on the chosen direction.
+ * This function continuously performs actions for the monster until the "q" command is received.
+ * It randomly selects a direction to move in, updates the monster's position if valid, and checks
+ * if the monster is in the same room as the player to initiate a fight.
+ *
+ * @param ptr A pointer to the game data structure (struct Data).
+ * @return NULL (void pointer) as it is a pthread-compatible function.
  */
-void processPlayerChoice(struct Data *d) {
-    sem_wait(&sem);
-    printf("\n%s", map[CURRENT_PLACE].description);
-    printf("\nPlayer info(i), search Room(f)\n");
-
-    printf("%s", map[CURRENT_PLACE].info);
-    scanf("%s", op);
-    sem_post(&sem);
-    if (strcmp(op, "i") == 0) {
-        playerResume(d);
-        return;
-    }
-    if (strcmp(op, "f") == 0) {
-        checkRoom(d);
-        return;
-    }
-    if (strcmp(op, "q") == 0) {
-        exit(0);
-    }
-    sleep(1);
-    if (d->player.map == d->monster[0].map){
-        return;
-    }
-    sem_wait(&sem);
-    changeRoom(op, d);
-    sem_post(&sem);
-}
-
-void checkRoom(struct Data *d) {
-    sem_wait(&sem);
-    printf("Searching.....\n");
-    sleep(2);
-
-    if (map[CURRENT_PLACE].treasure == 1) {
-        d->player.treasure = 1;
-        printf("A treasure was found!!!! Congratulations, you WIN.\n");
-        exit(0);
-    }
-    if (map[CURRENT_PLACE].object >= 0 && map[CURRENT_PLACE].object < 3) {
-        printf("You find a %s\n", d->objects[map[CURRENT_PLACE].object].name);
-        printf("Pick the new weapon? (y): ");
-        scanf("%s", op);
-        if(strcmp(op, "y") == 0) {
-            d->player.object = map[CURRENT_PLACE].object;
-            printf("You are carrying a %s\n", d->objects[d->player.object].name);
-        }
-    }
-    else {
-        printf("The room is empty!!!\n");
-    }
-    sem_post(&sem);
-}
-
-void playerResume(struct Data *d) {
-    sem_wait(&sem);
-    printf("PLAYER RESUME:\n");
-    printf("\tName: %s\n", d->player.name);
-    printf("\tEnergy: %d\n", d->player.energy);
-    if(d->player.object != -1) {
-        printf("\tWeapon: %s\n", d->objects[d->player.object].name);
-        printf("\tPower: %d", d->objects[d->player.object].power);
-    }
-    else{
-        printf("\tWeapon: None\n");
-        printf("\tPower: %d", 1);
-    }
-    sem_post(&sem);
-}
-
-void *threadPlayer(void *ptr) {
-    struct Data *d = (struct Data *) ptr;
-    while (strcmp(op, "q") != 0) {
-        sem_wait(&sem);
-        if (d->player.map == d->monster[0].map){
-            fightMonster(d);
-        }
-        sem_post(&sem);
-        processPlayerChoice(d);
-    }
-    return NULL;
-}
-
 void *threadMonster(void *ptr) {
+    // Cast the generic pointer to a pointer of struct Data
     struct Data *d = (struct Data *) ptr;
+    // Variables to represent movement direction and a pointer to check the direction
     int direction;
     int *checkDirection;
+    // Continuous loop until the "q" command is received
     while (strcmp(op, "q") != 0) {
+        // Generate a random number to determine the monster's movement direction
         direction = rand() % 6 + 1;
-
+        // Assign the appropriate map direction based on the random number
         if (direction == 1) checkDirection = &map[d->monster[0].map].north;
         else if (direction == 2) checkDirection = &map[d->monster[0].map].south;
         else if (direction == 3) checkDirection = &map[d->monster[0].map].east;
@@ -477,36 +511,56 @@ void *threadMonster(void *ptr) {
         else if (direction == 5) checkDirection = &map[d->monster[0].map].up;
         else if (direction == 6) checkDirection = &map[d->monster[0].map].down;
 
+        // If the selected direction is valid, update the monster's map position
         if (*checkDirection != -1) {
             d->monster[0].map = *checkDirection - 1;
             sleep(2);
         }
+        // Check if the player is in the same room as the monster
         if (d->player.map == d->monster[0].map){
+            // Acquire the semaphore to ensure exclusive access to shared data
             sem_wait(&sem);
+            // Check again if the player is still in the same room as the monster
             if (d->player.map == d->monster[0].map) {
+                // Initiate a fight
                 fightMonster(d);
             }
+            // Release the semaphore after accessing shared data
             sem_post(&sem);
         }
+        // Sleep for 5 seconds before the next move
         sleep(5);
     }
+    // Return NULL as it is a pthread-compatible function
     return NULL;
 }
 
+/**
+ * @brief Simulate a fight between the player and a monster.
+ *
+ * This function initiates a fight scenario, allowing the player to choose between fighting or fleeing.
+ * The outcome of the fight is determined based on the player and monster's power and energy levels.
+ *
+ * @param d A pointer to the game data structure (struct Data).
+ */
 void fightMonster(struct Data *d) {
+    // Display a message indicating the presence of a monster
     printf("\nUAAAAAAHHHHHH A MONSTER!!!");
 
+    // Calculate player's power based on equipped object or default power
     int playerPower;
     if(d->player.object != -1) {
         playerPower = d->objects[d->player.object].power;
     }
+    // Default power if no object is equipped
     else{
         playerPower = 1;
     }
-
+    // Prompt the player to choose whether to flee or fight
     printf("\nFugir? (y): ");
     scanf("%s", op);
 
+    // If the player chooses to run, decrease player's energy, display a message, and change room
     if(strcmp(op, "y") == 0){
         d->player.energy = d->player.energy - d->monster[0].power;
         printf("Perdeu %d de energia!\n", d->monster[0].power);
@@ -515,15 +569,89 @@ void fightMonster(struct Data *d) {
         changeRoom(op, d);
         return;
     }
+    // If the player chooses to fight(op != y), go to a fight scenario
     printf("WE WILL FIGHT!!!\n");
     sleep(2);
+    // Determine the outcome of the fight based on energy and power levels
     if(d->player.energy / d->monster[0].power <  d->monster[0].energy / playerPower){
         printf("YOU LOSE");
         exit(0);
     }
     else{
         printf("Congratulations!! You killed the monster.");
-        d->monster[0].energy = 50;
-        d->monster[0].map = rand() % 6 + 10;
+        d->monster[0].energy = 50; // Reset monster's energy
+        d->monster[0].map = rand() % 6 + 10; // Move monster to a new random room between 10 and 16.
     }
+}
+
+/**
+ * @brief Simulate searching a room for treasures or objects.
+ *
+ * This function simulates the player searching a room. It checks if the room contains a treasure,
+ * and if so, declares the player as the winner. If the room contains an object, it prompts the player
+ * to pick it up as a new weapon.
+ *
+ * @param d A pointer to the game data structure (struct Data).
+ */
+void checkRoom(struct Data *d) {
+    // Acquire the semaphore to ensure exclusive access to shared data
+    sem_wait(&sem);
+    // Display a message indicating that the player is searching
+    printf("Searching.....\n");
+    sleep(2);
+    // Check if the room contains a treasure
+    if (map[CURRENT_PLACE].treasure == 1) {
+        d->player.treasure = 1;
+        printf("A treasure was found!!!! Congratulations, you WIN.\n");
+        exit(0);
+    }
+    // Check if the room contains an object (weapon)
+    if (map[CURRENT_PLACE].object >= 0 && map[CURRENT_PLACE].object < 3) {
+        // Display information about the found object
+        printf("You find a %s\n", d->objects[map[CURRENT_PLACE].object].name);
+        // Prompt the player to pick up the new weapon
+        printf("Pick the new weapon? (y): ");
+        scanf("%s", op);
+        // If the player chooses to pick up the new weapon, update the player's object and display a message
+        if(strcmp(op, "y") == 0) {
+            d->player.object = map[CURRENT_PLACE].object;
+            printf("You are carrying a %s\n", d->objects[d->player.object].name);
+        }
+    }
+    // Display a message indicating that the room is empty
+    else {
+        printf("The room is empty!!!\n");
+    }
+    // Release the semaphore after accessing shared data
+    sem_post(&sem);
+}
+
+/**
+ * @brief Display a summary of the player's status.
+ *
+ * This function displays information about the player's name, energy level, and equipped weapon (if any).
+ * If the player has an equipped weapon, it also displays the power of the weapon.
+ *
+ * @param d A pointer to the game data structure (struct Data).
+ */
+void playerResume(struct Data *d) {
+    // Acquire the semaphore to ensure exclusive access to shared data
+    sem_wait(&sem);
+    // Display a summary of the player's status
+    printf("PLAYER RESUME:\n");
+    printf("\tName: %s\n", d->player.name);
+    printf("\tEnergy: %d\n", d->player.energy);
+    // Check if the player has an equipped weapon
+    if(d->player.object != -1) {
+        // Display information about the equipped weapon and its power
+        printf("\tWeapon: %s\n", d->objects[d->player.object].name);
+        printf("\tPower: %d", d->objects[d->player.object].power);
+    }
+    else{
+        // Display a message indicating that the player has no equipped weapon
+        printf("\tWeapon: None\n");
+        printf("\tPower: %d", 1); // Default power if no weapon is equipped
+    }
+    // Release the semaphore after accessing shared data
+    sem_post(&sem);
 }
